3884bd7446fa47fa9995ba965bee5713b935977d
Michelle Li <michelleyli123@gmail.com>
2021-07-13 18:54:27 -0400

    finished basic form structure for signup and login

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import Link from 'react-router-dom/Link';
 
 export default function Login() {
     return (
         <div>
-            Login
+            <h3>Login</h3>
+            <form>
+                <label>E-mail</label>
+                <input/>
+                <label>Password</label>
+                <input/>
+                <button>Login</button>
+                <p>Don't have an account? <Link to="/signup">Sign up here</Link></p>
+            </form>
         </div>
     )
 }
--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import Link from 'react-router-dom/Link';
 
 export default function Signup() {
     return (
         <div>
-            Signup
+            <h3>Signup</h3>
+            <form>
+                <label>Email</label>
+                <input/>
+                <label>Password</label>
+                <input/>
+                <label>Confirm Password</label>
+                <input/>
+                <button>Sign Up</button>
+                <p>Have an account? <Link to="/login">Login here</Link></p>
+            </form>
         </div>
     )
 }

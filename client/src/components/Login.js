0811a9280819db181fe0934c207d27a3a7812b40
Michelle Li <michelleyli123@gmail.com>
2021-07-13 19:14:40 -0400

    finished form functionality

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import {React, useRef} from 'react';
 import Link from 'react-router-dom/Link';
 
 export default function Login() {
+    const email = useRef();
+    const password = useRef();
+
+    function handleSubmit(){
+        
+    }
+
     return (
         <div>
             <h3>Login</h3>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>E-mail</label>
-                <input/>
+                <input ref={email} type="email"/>
                 <label>Password</label>
-                <input/>
+                <input ref={password} type="password"/>
                 <button>Login</button>
                 <p>Don't have an account? <Link to="/signup">Sign up here</Link></p>
             </form>
--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import { React, useRef } from 'react';
 import Link from 'react-router-dom/Link';
 
 export default function Signup() {
+    const email = useRef();
+    const password = useRef();
+    const confirm_password = useRef();
+
+    function handleSubmit(){
+        console.log(email.current.value, password.current.value, confirm_password.current.value)
+    }
+
     return (
         <div>
             <h3>Signup</h3>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Email</label>
-                <input/>
+                <input ref={email} type="email"/>
                 <label>Password</label>
-                <input/>
+                <input ref={password} type="password"/>
                 <label>Confirm Password</label>
-                <input/>
+                <input ref={confirm_password} type="password"/>
                 <button>Sign Up</button>
                 <p>Have an account? <Link to="/login">Login here</Link></p>
             </form>

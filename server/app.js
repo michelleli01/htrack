63d3d5548549330ca306ae326db0772db5a4912e
Michelle Li <michelleyli123@gmail.com>
2021-07-15 15:06:40 -0400

    basic login/signup functionality working

--- server/app.js
+++ server/app.js
@@ -1,51 +1,101 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
 const passport = require('passport');
-
-require('./models/db').connect("mongodb+srv://michelleli:denhop1009@users.ecemm.mongodb.net/node-auth");
-const authMiddleware = require('./passport/auth_middleware');
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const User = require('./models/user');
 
 const app = express();
-const PORT=8080;
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+mongoose.connect(
+    "mongodb+srv://michelleli:denhop1009@users.ecemm.mongodb.net/node-auth",
+    {
+        useNewUrlParser: true, 
+        useUnifiedTopology: true, 
+    },
+    () => {
+        console.log("[SERVER] mongoose is connected");
+    }
+);
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-// routes
-const authRouter = require('./routes/auth');
-
-app.use('/auth', authRouter);
-
-// set up passport
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+    cors({
+        origin: "http://localhost:3000",
+        credentials: true,
+    })
+);
+app.use(
+    session({
+        secret: "secretcode",
+        resave: true,
+        saveUninitialized: true,
+    })
+);
+app.use(cookieParser("secretcode"));
 app.use(passport.initialize());
-passport.use("local-signup", require('./passport/signup_passport'));
-passport.use("local-login", require('./passport/login_passport'));
+app.use(passport.session());
+require('./config/passport_config')(passport);
 
-
-//error handling
-app.use(function(req, res, next){
-    next(createError(404));
+app.post('/auth/login', (req, res, next)=>{
+    passport.authenticate('local', (err, user, info) => {
+        if(err) throw err;
+        if(!user) res.send("[SERVER] Account doesn't exist");
+        else{
+            req.logIn(user, (err) => {
+                if (err) throw err;
+                res.send("[SERVER] Successfully logged in");
+                console.log(req.user);
+            });
+        }
+    })(req, res, next);
 });
 
-app.use(function(err, req, res, next){
-    res.locals.message = err.message;
-    res.locals.error = req.app.get("env") === "development" ? err : {};
+app.post("/auth/signup", (req,res) => {
+    const { email, password } = req.body;
+    if(!email || !password){
+        res.send("[SERVER] Please enter all fields");
+    }
 
-    res.status(err.status || 500);
-    res.render('error')
+    if(password.length < 6){
+        res.send("[SERVER] Password must be at least 6 characters");
+    }
+    else{
+        User.findOne({ email: email })
+        .then(user => {
+            if(user){
+                res.send("[SERVER] Email already exists");
+            }
+            else{
+                const newUser = new User({
+                    email,
+                    password
+                });
+
+                bcrypt.genSalt(10, (err, salt) => {
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        if(err) console.log(err.message);
+                        newUser.password = hash;
+                        newUser
+                        .save()
+                        .then(user => {
+                            res.send("[SERVER] Account successfully created")
+                            res.redirect('/auth/login');
+                        })
+                        .catch(err => console.log(err.message));
+                    });
+                });
+            }
+        });
+    }
 });
 
-app.listen(PORT, ()=>{
-    console.log(`[SERVER] listening on: ${PORT}`)
+app.listen(8080, () => {
+    console.log("[SERVER] Server is running")
 })
 
 module.exports = app;
--- server/models/user.js
+++ server/models/user.js
@@ -1,38 +1,7 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-
-const UserSchema = new mongoose.Schema({
-    email: {
-        type: String, 
-        required: true, 
-        unique: true
-    },
-    password: {
-        type: String
-    }
+const user = new mongoose.Schema({
+    email: String, 
+    password: String
 });
 
-UserSchema.methods.comparePassword = function (password, callback){
-    bcrypt.compare(password, this.password, callback)
-}
-
-UserSchema.pre('save', function(next){
-    const user = this;
-
-    if(!user.isModified("password")) return next();
-    return bcrypt.genSalt((salt_error, salt) => {
-        if(salt_error){
-            return next(salt_error);
-        }
-        return bcrypt.hash(user.password, salt, (hash_error, hash) => {
-            if(hash_error){
-                return next(hash_error);
-            }
-
-            user.password = hash;
-            return next();
-        });
-    });
-});
-
-module.exports = mongoose.model("User", UserSchema);
+module.exports = mongoose.model("User", user);

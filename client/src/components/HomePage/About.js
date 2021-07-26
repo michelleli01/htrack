90ab56b8e1adce6ea341258ce4084474be6305bf
Michelle Li <michelleyli123@gmail.com>
2021-07-25 23:41:07 -0400

    small tweaks

--- client/src/components/HomePage/About.js
+++ client/src/components/HomePage/About.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-scroll";
 import { Container, Row, Col } from "react-bootstrap";
+import { AiFillGithub } from "react-icons/ai";
 
 import ABOUT_IMG_1 from "../../assets/ABOUT_IMG_1.jpg";
 
@@ -31,14 +32,20 @@ export default function About() {
                             </b>
                         </p>
                         <p className="about-text">
-                            I started this project in July 2021 to
-                            enhance and expand my knowledge in web development.
-                            This is my first major project which I worked on
-                            independently and in my free time. I used the
-                            ReactJS for frontend programming with ExpressJS
-                            and NodeJS for backend progamming in addition
-                            to MongoDB for storage.
+                            I started this project in July 2021 to enhance and
+                            expand my knowledge in web development. This is my
+                            first major project which I worked on independently
+                            and in my free time. I used the ReactJS for frontend
+                            programming with ExpressJS and NodeJS for backend
+                            progamming in addition to MongoDB for storage.
                         </p>
+                        <a
+                            href="https://www.github.com/michelleli01/htrack"
+                            className="about-link"
+                        >
+                            <AiFillGithub size="1.5em" color="#1c6e40" className="github-logo" />
+                            www.github.com/michelleli01/htrack
+                        </a>
                         <Link
                             to="contact"
                             spy={true}

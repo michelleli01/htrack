cb384c7961db1531877f3fcbcf3d0cc81deaab36
Michelle Li <michelleyli123@gmail.com>
2021-07-29 19:44:04 -0400

    start statistics from scratch

--- client/src/components/Secured/Statistics/Statistics.js
+++ client/src/components/Secured/Statistics/Statistics.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Pie from "./Pie";
 import "./Statistics.css";
 
 export default function Statistics() {
@@ -7,7 +6,6 @@ export default function Statistics() {
         <div className="statistics-container">
             <h3 className="statistics-header">Statistics</h3>
             <div className="data-container">
-                <Pie className="pie" />
             </div>
         </div>
     );
